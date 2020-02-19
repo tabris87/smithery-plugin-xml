655c630a14adf792096e9f27067270ac8c38f610
Adrian Marten <adrian.marten@msg.group>
2020-02-19 21:42:48 +0100

    Finished parsing, generating, and introduced first tests

--- test/imposing.test.js
+++ test/imposing.test.js
@@ -1,1 +1,37 @@
-test.todo('some test to be written in the future');
+const generator = require('../lib/generator');
+const parser = require('../lib/parser');
+const aRules = require('../lib/rules');
+//setup the heavy dependency chain
+const ImposerCL = require('featureJS/lib/Imposer');
+const ParserCL = require('featureJS/lib/Parser');
+const GeneratorCL = require('featureJS/lib/Generator');
+const RuleSetCL = require('featureJS/lib/RuleSet');
+
+const oImposer = new ImposerCL({
+    parser: new ParserCL(),
+    generator: new GeneratorCL(),
+    rules: new RuleSetCL()
+})
+
+oImposer.getParser().addParser(parser, 'xml');
+oImposer.getGenerator().addGenerator(generator, 'xml');
+oImposer.getRuleSet().addMultipleRules(aRules);
+
+function imposing(sBaseXML, sFeatureXML) {
+    const oASTBase = parser.parse(sBaseXML);
+    const oASTFeature = parser.parse(sFeatureXML);
+    const resultAST = oImposer.impose(oASTBase, oASTFeature, oImposer.getParser().getVisitorKeys('xml'));
+    return oImposer.getGenerator().generate(resultAST, 'xml');
+}
+
+function formatResult(sResultString) {
+    return generator.generate(parser.parse(sResultString));
+}
+
+test('Attribute overwrite', () => {
+    const sBaseXML = '<?xml version="1.0" encoding="utf-8"?>';
+    const sFeatureXML = '<?xml version="2.0"?>';
+    const sResultXML = formatResult('<?xml version="2.0" encoding="utf-8"?>');
+    debugger;
+    expect(imposing(sBaseXML, sFeatureXML)).toBe(sResultXML);
+});
