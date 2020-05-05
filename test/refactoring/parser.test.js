083ed73537f072ee1cb9e3ac951f402ff033d456
Adrian Marten <adrian.marten@msg.group>
2020-05-05 13:19:27 +0200

    extended tests

--- test/refactoring/parser.test.js
+++ test/refactoring/parser.test.js
@@ -54,6 +54,70 @@ const aTests = [{
             })()
             return node;
         }
+    }, {
+        title: 'Declaration with attributes to Tree',
+        xml: '<?xml version="1.0" encoding="utf-8"?>',
+        result: function () {
+            let node = new Node();
+            node.type = "document";
+            node.xmlType = "";
+            node.path = "document";
+            node.name = "root";
+            node.parent = undefined;
+            node.declaration = (() => {
+                var oDecl = new Node();
+                oDecl.type = "declaration";
+                oDecl.xmlType = "declaration";
+                oDecl.name = "declaration";
+                oDecl.parent = node;
+                oDecl.path = "document.declaration";
+                oDecl.attributes = [
+                    (() => {
+                        var oAtt1 = new Node();
+                        oAtt1.type = "attribute";
+                        oAtt1.xmlType = "attribute";
+                        oAtt1.name = "version";
+                        oAtt1.parent = oDecl;
+                        oAttl.path = "document.declaration.attribute";
+                        oAtt1.identifier = (() => {
+                            var oId1 = new Node();
+                            oId1.type = "identifier";
+                            oId1.xmlType = "";
+                            oId1.name = "version";
+                            oId1.parent = oAtt1;
+                            oId1.path = "document.declaration.attribute.identifier";
+                        })();
+                        oAtt1.value = (() => {
+                            var oValue1 = new Node();
+                            oValue1.type = "value";
+                            oValue1.xmlType = "";
+                            oValue1.name = "version";
+                            oValue1.parent = oAtt1;
+                            oValue1.path = "document.declaration.attribute.value";
+                            oValue1.value = "1.0";
+                        })()
+                    })(),
+                    //@Adrian TODO: hier weiter machen.
+                    (() => {
+                        var oAtt1 = new Node();
+                        oAtt1.type = "attribute";
+                        oAtt1.xmlType = "attribute";
+                        oAtt1.name = "version";
+                        oAtt1.parent = oDecl;
+                        oDecl.path = "document.declaration.attribute";
+                        oAtt1.identifier = (() => {
+                            var oId = new Node();
+                            oId.type = "identifier";
+                            oId.xmlType = "";
+                            oId.name = "version";
+                            oId.parent = oAtt1;
+                            oDecl.path = "document.declaration.attribute.identifier";
+                        })()
+                    })()
+                ]
+            })()
+            return node;
+        }
     }
 ]
 
