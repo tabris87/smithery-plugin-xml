d47e31efe9f4365072c956f33791e350ea73183d
Adrian Marten <adrian_marten@hotmail.com>
2020-05-12 07:33:37 +0200

    Added new test

--- test/refactoring/parser.test.js
+++ test/refactoring/parser.test.js
@@ -38,6 +38,87 @@ const aTests = [{
             })()];
             return node;
         }
+    },
+    {
+        title: 'Single Node with attributes to Tree',
+        xml: '<note content="1" aria-hidden="true"/>',
+        result: function () {
+            let node = new Node();
+            node.name = "root";
+            node.xmlType = "";
+            node.type = "document";
+            node.path = "document";
+            node.parent = undefined;
+            node.declaration = undefined;
+            node.elements = [(function () {
+                let ele = new Node();
+                ele.name = "note";
+                ele.parent = node;
+                ele.xmlType = "note";
+                ele.type = "element";
+                ele.path = "document.element";
+                ele.attributes = [
+                    (() => {
+                        var oAtt1 = new Node();
+                        oAtt1.type = "attribute";
+                        oAtt1.xmlType = "";
+                        oAtt1.name = "content";
+                        oAtt1.parent = ele;
+                        oAtt1.path = "document.declaration.attribute";
+                        oAtt1.identifier = (() => {
+                            var oId1 = new Node();
+                            oId1.type = "identifier";
+                            oId1.xmlType = "";
+                            oId1.name = "content";
+                            oId1.parent = oAtt1;
+                            oId1.path = "document.declaration.attribute.identifier";
+                            return oId1;
+                        })();
+                        oAtt1.value = (() => {
+                            var oValue1 = new Node();
+                            oValue1.type = "value";
+                            oValue1.xmlType = "";
+                            oValue1.name = "content";
+                            oValue1.parent = oAtt1;
+                            oValue1.path = "document.declaration.attribute.value";
+                            oValue1.value = "1";
+                            return oValue1;
+                        })()
+                        return oAtt1;
+                    })(),
+                    (() => {
+                        var oAtt2 = new Node();
+                        oAtt2.type = "attribute";
+                        oAtt2.xmlType = "";
+                        oAtt2.name = "aria-hidden";
+                        oAtt2.parent = ele;
+                        oAtt2.path = "document.declaration.attribute";
+                        oAtt2.identifier = (() => {
+                            var oId = new Node();
+                            oId.type = "identifier";
+                            oId.xmlType = "";
+                            oId.name = "aria-hidden";
+                            oId.parent = oAtt2;
+                            oId.path = "document.declaration.attribute.identifier";
+                            return oId;
+                        })();
+                        oAtt2.value = (() => {
+                            var oValue2 = new Node();
+                            oValue2.type = "value";
+                            oValue2.xmlType = "";
+                            oValue2.name = "aria-hidden";
+                            oValue2.parent = oAtt2;
+                            oValue2.path = "document.declaration.attribute.value";
+                            oValue2.value = "true";
+                            return oValue2;
+                        })()
+                        return oAtt2;
+                    })()
+                ]
+                return ele;
+            })()];
+            return node;
+        }
     }, {
         title: 'Simple Declaration to Tree',
         xml: '<?xml?>',
